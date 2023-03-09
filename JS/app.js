87e2e854d37420e61c0ca3b568a6bac9637c92ff
“Ricardo1435” <ricantog1435@gmail.com>
2023-03-09 17:15:54 -0600

    Pagina simple AWS

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+$( document ).ready( function() {
+    Swal.fire({
+        position: 'top-end',
+        title: '¡Bienvenido!',
+        showConfirmButton: false,
+        timer: 1500
+      })
+} )
